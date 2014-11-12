5d11b122853d320ac47ef41a9875789767db2bbe
Marek Strzała <marekwstrzala@gmail.com>
2014-11-12 16:13:29 +0100

    add comments and some improvements for tests

--- src/test/java/com/marekag/spring/service/PlayerManagerTest.java
+++ src/test/java/com/marekag/spring/service/PlayerManagerTest.java
@@ -2,6 +2,7 @@ package com.marekag.spring.service;
 
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
@@ -49,6 +50,7 @@ public class PlayerManagerTest {
 		
 		playerManager.addPlayer(player);
 		
+		assertNotNull(playerManager.getPlayer(player));
 		Player newPlayer = playerManager.getPlayer(player);
 		
 		assertEquals(NAME_1, newPlayer.getName());
@@ -61,6 +63,8 @@ public class PlayerManagerTest {
 	@Test
 	public void updatePlayerTest() {
 		
+		
+		// dodanie dwóch piłkarzy
 		Player player = new Player();
 		player.setName(NAME_1);
 		player.setLastName(LASTNAME_1);
@@ -69,26 +73,45 @@ public class PlayerManagerTest {
 		
 		playerManager.addPlayer(player);
 		
-		Player newPlayer = playerManager.getPlayer(player);
+		Player player2 = new Player();
+		player2.setName(NAME_1);
+		player2.setLastName(LASTNAME_1);
+		player2.setAge(AGE_1);
+		player2.setPosition(POSITION_1);
 		
+		playerManager.addPlayer(player2);
+		
+		assertNotNull(playerManager.getPlayer(player));
+		Player newPlayer = playerManager.getPlayer(player);
+		// pobranie pierwszego piłkarza
 		assertEquals(NAME_1, newPlayer.getName());
 		assertEquals(LASTNAME_1, newPlayer.getLastName());
 		assertEquals(AGE_1, newPlayer.getAge());
 		assertEquals(POSITION_1, newPlayer.getPosition());
-		
+		// zmiana pozycji
 		newPlayer.setPosition(POSITION_2);
 		
+		assertNotNull(playerManager.getPlayer(newPlayer));
 		Player newestPlayer = playerManager.getPlayer(newPlayer);
-		
+		// sprawdzenie czy pierwszy się zmienił
 		assertEquals(NAME_1, newestPlayer.getName());
 		assertEquals(LASTNAME_1, newestPlayer.getLastName());
 		assertEquals(AGE_1, newestPlayer.getAge());
 		assertEquals(POSITION_2, newestPlayer.getPosition());
+		
+		assertNotNull(playerManager.getPlayer(player2));
+		Player newPlayer2 = playerManager.getPlayer(player2);
+		// sprawdzenie czy drugi się nie zmienił
+		assertEquals(NAME_1, newPlayer2.getName());
+		assertEquals(LASTNAME_1, newPlayer2.getLastName());
+		assertEquals(AGE_1, newPlayer2.getAge());
+		assertEquals(POSITION_1, newPlayer2.getPosition());
 	}
 	
 	@Test
 	public void deletePlayerTest() {
 		
+		//dodanie dwóch piłkarzy
 		Player player = new Player();
 		player.setName(NAME_1);
 		player.setLastName(LASTNAME_1);
@@ -97,17 +120,36 @@ public class PlayerManagerTest {
 		
 		playerManager.addPlayer(player);
 		
-		Player newPlayer = playerManager.getPlayer(player);
+		Player player2 = new Player();
+		player2.setName(NAME_2);
+		player2.setLastName(LASTNAME_2);
+		player2.setAge(AGE_2);
+		player2.setPosition(POSITION_2);
 		
+		playerManager.addPlayer(player2);
+		
+		assertNotNull(playerManager.getPlayer(player));
+		Player newPlayer = playerManager.getPlayer(player);
+		// sprawdzenie czy pierwszy jest w bazie
 		assertEquals(NAME_1, newPlayer.getName());
 		assertEquals(LASTNAME_1, newPlayer.getLastName());
 		assertEquals(AGE_1, newPlayer.getAge());
 		assertEquals(POSITION_1, newPlayer.getPosition());
-		
+		// usunięcie pierwszego z bazy
 		playerManager.deletePlayer(newPlayer);
-
+		// ponowne sprawdzenie czy pierwszy jest w bazie
 		assertNull(playerManager.getPlayer(newPlayer));
 		assertNull(playerManager.getPlayer(player));
+		
+		// sprawdzenie czy możemy pobrać drugiego
+		assertNotNull(playerManager.getPlayer(player2));
+		Player newPlayer2 = playerManager.getPlayer(player2);
+		
+		// sorawdzenie czy to na pewno ten rekord
+		assertEquals(NAME_2, newPlayer2.getName());
+		assertEquals(LASTNAME_2, newPlayer2.getLastName());
+		assertEquals(AGE_2, newPlayer2.getAge());
+		assertEquals(POSITION_2, newPlayer2.getPosition());
 
 	}
 	
@@ -161,6 +203,7 @@ public class PlayerManagerTest {
 		
 		playerManager.addPlayer(player2);
 				
+		assertNotNull(playerManager.getPlayer(player.getId()));
 		Player newPlayer = playerManager.getPlayer(player.getId());
 		newPlayer.setName(NAME_1);
 		newPlayer.setLastName(LASTNAME_1);
